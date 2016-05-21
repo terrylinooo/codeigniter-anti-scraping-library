986bec2322df10db6f22314f5a72fb248d077bdd
Terry Lin <some.lin@gmail.com>
2016-05-21 19:09:24 +0800

    Create footer.php
    Put CI_AntiScraping() in the footer of template.

---
+++ application/views/footer.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+	</main>
+
+	<footer id="site-footer" role="contentinfo">
+	
+	</footer>
+
+<?= CI_AntiScraping() ?>
+</body>
+</html>
